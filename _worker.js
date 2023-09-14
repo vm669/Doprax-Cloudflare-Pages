fd4d9a75f40a0cead07865be38f2191645f2ebe6
vm669 <82027738+vm669@users.noreply.github.com>
2023-09-14 13:08:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = '85597d55-2f54-4ab4-997c-114eb993c705.id.repl.co'
+      url.hostname = 'ygkkkrnsx.eu-de-aalkfytirf-fas.dopraxrocks.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
